c72c9e6caaa72506d7514fc0e089c39efbeec8dd
Karye <karye@users.noreply.github.com>
2022-09-01 09:37:20 +0200

    Skapa grundkod och intro till cw

---
+++ Intro/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Karim!");
+            Console.WriteLine("Jag är bäst på Battlefield 1942");
+        }
+    }
+}
