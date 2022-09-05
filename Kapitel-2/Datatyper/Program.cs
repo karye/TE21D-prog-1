b8675e0533fc6842d5c1756419fe251ac82268dc
Karye <karye@users.noreply.github.com>
2022-09-05 09:35:10 +0200

    Tränat på datatyper int o double

---
+++ Kapitel-2/Datatyper/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Datatyper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Berätta om programmet
+            Console.Clear();
+            Console.WriteLine("Program som räknar ut ålder vid examen");
+
+            // Läs in namnet
+            Console.Write("Ange ditt namn: ");
+            string namn = Console.ReadLine();
+            
+            // Läs in ålder
+            Console.Write("Ange din ålder: ");
+            string ålderString = Console.ReadLine(); // Tex "17"
+            
+            // Konvertera från string till heltal
+            int ålder = int.Parse(ålderString); // Tex 17
+
+            // Räkna ut ålder vid examen
+            ålder = ålder + 2;
+
+            // Skriv ut resultatet
+            //Console.WriteLine(namn + ", du är " + ålder + " år när du tar examen 2024");
+            Console.WriteLine($"{namn} du är {ålder} år när du tar examen 2024");
+        }
+    }
+}
---
+++ Kapitel-2/Labb1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Labb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Program som räknar hur mycket mer Duplantis hoppar än andra");
+
+            // Läs in höjden i meter
+            Console.Write("Ange din hopphöjd i meter: ");
+            string höjdString = Console.ReadLine(); // Tex "5,5"
+            
+            // Konvertera till decimaltal
+            double höjd = double.Parse(höjdString); // 5.5
+
+            // Räkna ut hur mycket högre Duplantis hoppar
+            double höjdSkillnad = 6.25 - höjd;
+
+            // Skriv ut resultatet
+            Console.WriteLine($"Du hoppar {höjdSkillnad} mindre än Duplantis");
+        }
+    }
+}
