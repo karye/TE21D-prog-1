68cd261c9381161bb0c6fe436479607c43eecc6d
Karye <karye@users.noreply.github.com>
2022-09-08 13:47:23 +0200

    Klar med labben

--- Kapitel-2/Labb2/Program.cs
+++ Kapitel-2/Labb2/Program.cs
@@ -6,7 +6,24 @@ namespace Labb2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            Console.WriteLine("Program för att räkna ut din BMI");
+
+            // Läs in vikt i kg
+            Console.Write("Hur mycket väger du? (kg) ");
+            string viktString = Console.ReadLine();
+            double vikt = double.Parse(viktString);
+
+            // Läs in längd i meter
+            Console.Write("Hur lång är du? (m) ");
+            string längdString = Console.ReadLine();
+            double längd = double.Parse(längdString);
+
+            // Räkna ut BMI
+            double bmi = vikt / (längd * längd);
+
+            // Skriv ut resultatet
+            Console.WriteLine($"Din bmi är {bmi}");
         }
     }
 }
