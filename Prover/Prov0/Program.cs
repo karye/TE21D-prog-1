af6e688c4cde838457918d0d480af8d397c52a91
Karye <karye@users.noreply.github.com>
2022-09-26 09:30:59 +0200

    Rättat provet

---
+++ Prover/Prov0/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Prov0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Var provet lätt? (j/n) ");
+
+            // Programloop
+            int försök = 0;
+            while (true)
+            {
+                // Räknar antal gånger
+                försök++;
+
+                // Läser in svaret
+                string svar = Console.ReadLine();
+                
+                // Kontrollera om det var rätt svar
+                if (svar == "j")
+                {
+                    Console.WriteLine($"Bra svarat! Du klarade på {försök} försök");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Fel svar, försök igen!");
+                }
+
+                if (försök == 5)
+                {
+                    Console.WriteLine("Du får inga fler försök!");
+                    break;
+                }
+            }
+
+        }
+    }
+}
