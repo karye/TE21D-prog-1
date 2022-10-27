23f840ed258bb2480bddb8e22975efe12d2ff3ee
karye <karye.webb@gmail.com>
2022-10-27 16:17:19 +0200

    Tränat på array

---
+++ Kapitel-5/Morse/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Morse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Från svenska till Morsekod!");
+
+            // 2 tabeller: svenska alfabetet och morsealfabetet
+            string alfabetet = "ABCDEFGHIJKLMNIOPQRSTUVWXYZÅÄÖ ";
+            string[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", 
+                              "....", "..", ".---", "-.-", ".-..", "--", "-.", 
+                              "---", ".--.", "--.-", ".-.", "...", "-", ".--", 
+                              "...-", ".--", "-..-", "-.--", "--..", ".--.-", 
+                              ".-.-", "---.", "/"};
+
+            // Ange ett meddelande
+            Console.Write("Ange ett meddelande: ");
+            string meddelande = Console.ReadLine();
+            
+            // Loopa igenom meddelandet
+            string meddelandeMorse = "";
+            foreach (var tecken in meddelande)  // tecken är en 'char'
+            {
+                // Ersätter till versaler
+                string bokstav = tecken.ToString().ToUpper();
+
+                // Leta efter position (index) i alfabetet
+                int index = alfabetet.IndexOf(bokstav);
+
+                //Console.WriteLine($"{bokstav} ligger på index {index}");
+
+                // Skriv ut morsekoden
+                //Console.WriteLine(morse[index]);
+
+                // Sätt samman morsemeddelandet
+                meddelandeMorse += morse[index];
+            }
+
+            // Skriv ut hela meddelandet i morsekod
+            Console.WriteLine(meddelandeMorse);
+        }
+    }
+}
---
+++ Kapitel-5/TextEmoji/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using J3QQ4;
+
+namespace TextEmoji
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine(Emoji.Baby);
+        }
+    }
+}
