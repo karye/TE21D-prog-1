fe11e1005bbdb52846ee7cfdca05baa57e525365
karye <karye.webb@gmail.com>
2022-11-10 16:33:21 +0100

    Tränat på flödesschema och metoder

---
+++ Kapitel-6/Intro/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            SkrivUtKvadrat(9);
+        }
+
+        // EN metod för att skriva ut kvadraten
+        static void SkrivUtKvadrat(int tal)
+        {
+            Console.WriteLine($"Kvadraten på {tal} är {tal * tal}");
+        }
+
+        // Metod som visar en meny
+        static void VisaMeny()
+        {
+            Console.WriteLine("1. Räkna ut kvadraten");
+            Console.WriteLine("2. Räkna ut Kuben");
+            Console.WriteLine("3. Avsluta programmet");
+        }
+    }
+}
---
+++ Kapitel-6/Uppgift-6.2/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 
+Skapa ett program som innehåller metoden SkrivBaklänges(string meddelande). Metoden ska skriva ut parametern meddelande baklänges. Anropa metoden minst tre gånger i ditt program.
+ */
+using System;
+
+namespace Uppgift_6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SkrivBaklänges("Hello World!");
+        }
+
+        static void SkrivBaklänges(string meddelande)
+        {
+            for (int i = meddelande.Length - 1; i >= 0; i--)
+            {
+                Console.Write(meddelande[i]);;
+            }
+        }
+    }
+}
