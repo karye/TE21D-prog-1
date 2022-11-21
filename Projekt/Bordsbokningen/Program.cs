15a0d53de27b7b2d4ae542ab0fb844fabf65f6ca
karye <karye.webb@gmail.com>
2022-11-21 09:45:39 +0100

    Början till ett konsolspel

---
+++ Projekt/Bordsbokningen/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace Bordsbokningen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initiera några variabler
+            string filnamn = "centralbord.csv";
+            string tomBokning = "0, Inga gäster";
+            int antalBord = 8;
+
+            // Skapa array för att lagra bokningar
+            string[] bordsInformation = new string[antalBord];
+
+            // Information om programmet
+            Console.WriteLine("Välkommen till Centralrestaurangens bordshanterare!");
+
+            // Läs från textfilen och lagra i aen array
+            if (File.Exists(filnamn))
+            {
+                bordsInformation = File.ReadAllLines(filnamn);
+            }
+            else
+            {
+                // Fyll filen med 8 tomma bord
+                // Alternativ 1
+                File.WriteAllText(filnamn, tomBokning + "\n" +
+                                tomBokning + "\n" +
+                                tomBokning + "\n" +
+                                tomBokning + "\n" +
+                                tomBokning + "\n" +
+                                tomBokning + "\n" +
+                                tomBokning + "\n" +
+                                tomBokning + "\n"
+                                 );
+            }
+
+            // Skriv ut menyn
+            Console.WriteLine("\n1. Visa alla bord");
+            Console.WriteLine("2. Boka bord");
+            Console.WriteLine("3. Avboka bord");
+            Console.WriteLine("4. Avsluta");
+            Console.Write("Välj ett alternativ: ");
+            string val = Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Projekt/Snake/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using J3QQ4;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Hello Snake! ");
+
+            Console.WriteLine(Emoji.Snake + Emoji.Alien);
+
+            // Virtuell rad
+            int[] raden = { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 };
+            int vitPos = 3;
+
+            while (true)
+            {
+                // Töm vita rutan
+                raden[vitPos] = 0;
+
+                // Läs in en tangent
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.LeftArrow)
+                {
+                    vitPos--;
+                }
+                else if (key.Key == ConsoleKey.RightArrow)
+                {
+                    vitPos++;
+                }
+
+                // Rita nya positionen på vita rutan
+                raden[vitPos] = 1;
+
+                // Rita ut virtuella raden
+                Console.Clear();
+                for (int i = 0; i < raden.Length; i++)
+                {
+                    if (raden[i] == 0)
+                    {
+                        Console.Write(Emoji.Black_Large_Square);
+                    }
+                    else
+                    {
+                        Console.Write(Emoji.White_Large_Square);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
