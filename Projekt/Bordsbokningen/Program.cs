c99560549b419bc675d38312e1fde589f4fa70ff
karye <karye.webb@gmail.com>
2022-11-24 16:28:59 +0100

    Fortsatt med projektet

--- Projekt/Bordsbokningen/Program.cs
+++ Projekt/Bordsbokningen/Program.cs
@@ -27,7 +27,7 @@ namespace Bordsbokningen
             {
                 // Fyll filen med 8 tomma bord
                 // Alternativ 1
-                File.WriteAllText(filnamn, tomBokning + "\n" +
+                /* File.WriteAllText(filnamn, tomBokning + "\n" +
                                 tomBokning + "\n" +
                                 tomBokning + "\n" +
                                 tomBokning + "\n" +
@@ -35,7 +35,17 @@ namespace Bordsbokningen
                                 tomBokning + "\n" +
                                 tomBokning + "\n" +
                                 tomBokning + "\n"
-                                 );
+                                 ); */
+
+                // Alternativ 2
+                // Fyll arrayens alla platser med texten tombokning
+                for (int i = 0; i < antalBord; i++)
+                {
+                    bordsInformation[i] = tomBokning;
+                }
+                // Spara ned hela arrayen i textfilen
+                File.WriteAllLines(filnamn, bordsInformation);
+                Console.WriteLine("Fil med bokningar hittades ej. Ny fil skapad.");
             }
 
             // Skriv ut menyn
@@ -45,7 +55,51 @@ namespace Bordsbokningen
             Console.WriteLine("4. Avsluta");
             Console.Write("Välj ett alternativ: ");
             string val = Console.ReadLine();
-            
+
+            // Hanterar användarens val
+            switch (val)
+            {
+                case "1":
+                    // Visa alla bord
+                    for (int i = 0; i < antalBord; i++)
+                    {
+                        if (bordsInformation[i] == tomBokning)
+                        {
+                            // Ingen bokning
+                            Console.WriteLine($"Bord {i + 1} - Inga gäster");
+                        }
+                        else
+                        {
+                            // Bokning finns
+                            string rad = bordsInformation[i];
+
+                            // Dela upp raden efter ", "
+                            string[] delar = rad.Split(", ");
+                            string antalGäster = delar[0];
+                            string namn = delar[1];
+
+                            Console.WriteLine($"Bord {i + 1} - Namn: {namn}, antal gäster: {antalGäster}");
+                        }
+
+                    }
+                    break;
+
+                case "2":
+                    // Ändra bokning
+                    break;
+
+                case "3":
+                    // Ta bort bokning
+                    break;
+
+                case "4":
+                    // Avsluta
+                    break;
+
+                default:
+                    Console.WriteLine("Var vänlig välj 1-4");
+                    break;
+            }
         }
     }
 }
