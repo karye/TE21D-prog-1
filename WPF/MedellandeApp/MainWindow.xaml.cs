827dfeb2ff6413de98a593025a4c2414159c1660
karye <karye.webb@gmail.com>
2023-03-13 09:40:15 +0100

    Tränat på WPF, mail och regex

--- WPF/MedellandeApp/MainWindow.xaml.cs
+++ WPF/MedellandeApp/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Mail;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace MedellandeApp
 {
@@ -23,6 +26,48 @@ namespace MedellandeApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // Hantera klick Skicka
+        private void KlickSkicka(object sender, RoutedEventArgs e)
+        {
+            // Läsa av epostadressen
+            string epost = rutaEpost.Text;
+
+            // Läsa av meddelandet
+            string texten = rutaTexten.Text;
+
+            // Hälsocheck!
+            // Vi kontrollerar att epostadressen har rätt format
+            string regexEpost = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+            if (Regex.IsMatch(epost, regexEpost))
+            {
+                if (texten != "")
+                {
+                    // Inloggningsuppgifter
+                    string id = "karim@gmail.com";
+                    string lösen = "....";
+
+                    // Input från amvändaren verkar ok!
+                    SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                    smtp.EnableSsl = true;
+                    smtp.Credentials = new NetworkCredential(id, lösen);
+
+                    // Nu skicka vi...
+                    smtp.Send(epost, id, "rubrik", texten);
+
+                    // Bekräftelse till användaren
+                    rutaStatus.Text = "Meddelandet skickat!";
+                }
+                else
+                {
+                    rutaStatus.Text = "Textmeddelandet saknas!";
+                }
+            }
+            else
+            {
+                rutaStatus.Text = "Epostadressen saknas!";
+            }
         }
     }
 }
